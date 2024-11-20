ba3f7bb6ff96c0d4eec2ecb47f05a5792f1dbcc2
aaron <aaronthien052401@gmail.com>
2024-11-20 15:43:26 -0500

    Co-authored-by: Joyce Oh <joyceoh@users.noreply.github.com>

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors'
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log('Error; ', err);
+    res.status(500).json({ messsage: 'Internal Server Error ', error: err.message })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
