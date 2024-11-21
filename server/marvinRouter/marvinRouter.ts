5f39cd1510c2b884944bb862982553a5215a0630
pauluhlenkott <pauluhlenkott@gmail.com>
2024-11-20 16:50:51 -0800

    Moved cache controller

--- server/marvinRouter/marvinRouter.ts
+++ server/marvinRouter/marvinRouter.ts
@@ -1,10 +1,19 @@
-import express, { Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import userController from '../userController/userController';
+import { queryOpenAIChat } from '../controllers/aiController';
 
 const marvinRouter = express.Router();
 
-marvinRouter.post('/chat', userController.getString, aiController.handleResponse, (req: Request, res: Response) => {
-    return res.status(200).json({userResponse: res.locals.userResponse, aiResponse: res.locals.aiResponse})
-});
+marvinRouter.post(
+  '/chat',
+  userController.getString,
+  queryOpenAIChat,
+  (req: Request, res: Response) => {
+    return res.status(200).json({
+      userResponse: res.locals.userResponse,
+      aiResponse: res.locals.aiResponse,
+    });
+  }
+);
 
-export default marvinRouter
+export default marvinRouter;
